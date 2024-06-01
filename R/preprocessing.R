03d8e9ac0701cdac1b483a5afb7c4f15d0885985
Carol-seven <542046605@qq.com>
2024-06-01 02:00:56 -0400

    preprocessing

---
+++ R/preprocessing.R
@@ -0,0 +1,86 @@
+#'
+#' Loading and reformatting of metabolomics data
+#'
+#' @description
+#' Read a data file, select columns necessary for analysis, and return the reformatted data.
+#'
+#' @param fileName The name of the .csv file containing metabolomics data (including the
+#' path to the file, if needed).
+#'
+#' @param dataSet The raw data set, if already loaded in R.
+#'
+#' @param names A vector of strings (default = c("gender", "treatment", "replicate"))
+#' specifying the names of the attribute columns.
+#'
+#' @details
+#' The function executes the following:
+#' \enumerate{
+#' \item Reads the file.
+#' \item Provides summary statistics and a histogram of all values reported in the data set.
+#' \item Re-formats the data to present individual compounds as columns.
+#' \item Stores the data as a \code{data.frame} and prints the levels of attributes to the
+#' user.
+#' }
+#'
+#' @import dplyr
+#' @import ggplot2
+#' @import tidyr
+#' @importFrom utils read.csv
+#'
+#' @returns A 2d dataframe.
+#'
+#' @autoglobal
+#'
+#' @export
+
+preprocessing <- function(fileName, dataSet = NULL,
+                          names = c("gender", "treatment", "replicate")) {
+  if (missing(fileName)) {
+    if (is.null(dataSet)) {
+      stop("Either 'fileName' or 'dataSet' must be provided.")
+    }
+  } else {
+    ## read in the metabolomics quantitative csv file
+    dataSet <- read.csv(fileName)
+  }
+
+  ## long dataSet table
+  dataSet.l <- dataSet %>%
+    pivot_longer(cols = -Compound) %>%
+    mutate(value = ifelse(value == 0, NA, value))
+
+  ## reformatted data
+  result <- dataSet.l %>%
+    pivot_wider(id_cols = name, names_from = Compound, values_from = value) %>%
+    separate(name, into = names, sep = "_") %>%
+    as.data.frame()
+
+  ## generate a histogram of the log2-transformed values for full raw data set
+  value <- data.frame(value = log2(dataSet.l$value))
+  plot <- ggplot(value) +
+    geom_histogram(aes(x = value),
+                   breaks = seq(floor(min(value, na.rm = TRUE)),
+                                ceiling(max(value, na.rm = TRUE)), 1),
+                   color = "black", fill = "gray") +
+    scale_x_continuous(breaks = seq(floor(min(value, na.rm = TRUE)),
+                                    ceiling(max(value, na.rm = TRUE)), 2)) +
+    labs(title = "Histogram of Full Raw Data Set",
+         x = expression("log"[2]*"(Data)"), y = "Frequency") +
+    theme_bw() +
+    theme(plot.title = element_text(hjust = 0.5))
+  print(plot)
+
+  ## print summary statistics for full raw data set
+  cat("Summary of Full Data Signals (Raw):\n")
+  print(summary(dataSet.l$value))
+  cat("\n")
+
+  ## print levels of attributes
+  for (k in 1:length(names)) {
+    cat(paste0("Levels of ", names[k], ":"),
+        eval(parse(text = paste0("unique(result$", names[k], ")"))), "\n")
+  }
+  cat("\n")
+
+  return(result)
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# metastat <img src="man/figures/logo.png" align="right" alt="" width="210">
+
+## Installation
+
+You can install the development version of **metastat** from
+[GitHub](https://github.com/) with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("uconn-scs/metastat")
+```
